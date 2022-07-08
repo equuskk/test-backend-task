7216c062a2ec5170d0901c45fa84c65a3060971b
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2022-07-08 22:03:36 +0300

    Add base endpoint classes

---
+++ TestBackendTask/Endpoints/Abstractions/BaseEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+
+namespace TestBackendTask.Endpoints.Abstractions;
+
+public abstract class BaseEndpoint
+{
+    public string Path { get; init; }
+    public string Method { get; init; }
+
+    protected HttpListenerContext _context;
+
+    public void Execute(HttpListenerContext context)
+    {
+        _context = context;
+        Handle(_context.Request);
+    }
+
+    public abstract void Send(int statusCode, object? response);
+    public abstract void Handle(HttpListenerRequest request);
+
+    public void SendOk(object? response)
+    {
+        Send(200, response);
+    }
+}
---
+++ TestBackendTask/Endpoints/Abstractions/Endpoint.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
+
+namespace TestBackendTask.Endpoints.Abstractions;
+
+public class Endpoint : BaseEndpoint
+{
+    public override void Send(int statusCode, object? response)
+    {
+        _context.Response.StatusCode = statusCode;
+        if(response is null)
+        {
+            _context.Response.Close();
+            return;
+        }
+
+        if(response.GetType().IsGenericType)
+        {
+            var serializedObject = JsonSerializer.Serialize(response);
+            var responseBytes = Encoding.UTF8.GetBytes(serializedObject);
+            _context.Response.AddHeader("Content-Type", MediaTypeNames.Application.Json);
+            _context.Response.OutputStream.Write(responseBytes);
+        }
+        else
+        {
+            _context.Response.OutputStream.Write(Encoding.UTF8.GetBytes(response.ToString()));
+        }
+
+        _context.Response.Close();
+    }
+
+    public override void Handle(HttpListenerRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
